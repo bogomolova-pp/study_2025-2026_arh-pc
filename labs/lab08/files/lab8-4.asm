edaf223d4fdacee41371448597ccf5c90d3b0412
bogomolova-pp <1032253562@pfur.ru>
2025-11-21 21:13:26 +0300

    Add files via upload

---
+++ labs/lab08/files/lab8-4.asm
@@ -0,0 +1,34 @@
+%include 'in_out.asm'
+
+SECTION .data
+msg1 db "Функция: f(x)=10*x-5", 10, 0
+msg2 db "Результат: ", 0
+
+SECTION .text
+global _start
+
+_start:
+    pop ecx
+    pop edx
+    sub ecx, 1
+    mov esi, 0
+
+    mov eax, msg1
+    call sprint
+
+next:
+    cmp ecx, 0
+    jz _end
+    pop eax
+    call atoi
+    imul eax, eax, 10
+    sub eax, 5
+    add esi, eax
+    loop next
+
+_end:
+    mov eax, msg2
+    call sprint
+    mov eax, esi
+    call iprintLF
+    call quit
